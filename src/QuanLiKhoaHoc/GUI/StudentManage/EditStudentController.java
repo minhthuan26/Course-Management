9fc44ff20a20fa9610e4c5cfc38e0518393436e9
minhthuan <minhthuan26042001@gmail.com>
2022-10-04 19:00:08 +0700

    Hoàn thành chức năng sửa trên quản lí học viên và giảng viên

---
+++ src/QuanLiKhoaHoc/GUI/StudentManage/EditStudentController.java
@@ -0,0 +1,154 @@
+package QuanLiKhoaHoc.GUI.StudentManage;
+
+import QuanLiKhoaHoc.BUS.StudentManage.StudentBUS;
+import QuanLiKhoaHoc.DTO.Person;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class EditStudentController implements Initializable {
+
+    private final StudentBUS studentBus = new StudentBUS();
+
+    @FXML
+    private AnchorPane windowPane;
+    @FXML
+    private AnchorPane wrapPane;
+    @FXML
+    private Button saveBtn;
+    @FXML
+    private Button cancelBtn;
+    @FXML
+    private TextField idTxtField;
+    @FXML
+    private TextField firstNameTxtField;
+    @FXML
+    private TextField lastNameTxtField;
+    @FXML
+    private DatePicker birthDatePicker;
+    @FXML
+    private TextField phoneTxtField;
+
+    private static double xOffset = 0;
+    private static double yOffset = 0;
+    private Stage primaryStage;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Handle();
+        setDefault();
+    }
+
+    public void Handle(){
+        windowPane.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                primaryStage = (Stage) wrapPane.getScene().getWindow();
+                xOffset = primaryStage.getX() - event.getScreenX();
+                yOffset = primaryStage.getY() - event.getScreenY();
+            }
+        });
+
+        windowPane.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                primaryStage = (Stage) wrapPane.getScene().getWindow();
+                primaryStage.setX(event.getScreenX() + xOffset);
+                primaryStage.setY(event.getScreenY() + yOffset);
+            }
+        });
+
+        cancelBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                primaryStage = (Stage) cancelBtn.getScene().getWindow();
+                primaryStage.close();
+            }
+        });
+
+        saveBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                String firstName, lastName, phoneNumber;
+                LocalDate dateOfBirth;
+                firstName = firstNameTxtField.getText().trim();
+                lastName = lastNameTxtField.getText().trim();
+                dateOfBirth = birthDatePicker.getValue();
+                phoneNumber = phoneTxtField.getText().trim();
+                if(firstName.equals("") || lastName.equals("") || phoneNumber.equals("")){
+                    errorAlert("Lỗi", "Tất cả các trường không được để trống");
+                }
+                else{
+                    Person teacher = new Person(
+                            Integer.parseInt(idTxtField.getText()),
+                            firstName,
+                            lastName,
+                            "",
+                            phoneNumber,
+                            dateOfBirth,
+                            ""
+                            );
+                    teacher = studentBus.updateStudent(teacher);
+                    if(teacher!=null){
+                        alert("Thông báo", "Cập nhật thành công");
+                        primaryStage = (Stage) saveBtn.getScene().getWindow();
+                        primaryStage.close();
+                    }
+                    else{
+                        errorAlert("Lỗi", "Có lỗi xảy ra");
+                    }
+                }
+            }
+        });
+    }
+
+    private void errorAlert(String title, String Message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        DialogPane root = alert.getDialogPane();
+        root.getStylesheets().add(Objects.requireNonNull(getClass().getResource("../Main/main.css")).toExternalForm());
+        root.getScene().setFill(Color.TRANSPARENT);
+        Stage dialogStage = (Stage) root.getScene().getWindow();
+        dialogStage.initStyle(StageStyle.TRANSPARENT);
+        alert.setContentText(Message);
+        alert.setHeaderText(null);
+        ButtonType okBtn = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
+        alert.getButtonTypes().setAll(okBtn);
+        alert.show();
+    }
+
+    private void alert(String title, String Message){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        DialogPane root = alert.getDialogPane();
+        root.getStylesheets().add(Objects.requireNonNull(getClass().getResource("../Main/main.css")).toExternalForm());
+        root.getScene().setFill(Color.TRANSPARENT);
+        Stage dialogStage = (Stage) root.getScene().getWindow();
+        dialogStage.initStyle(StageStyle.TRANSPARENT);
+        alert.setContentText(Message);
+        alert.setHeaderText(null);
+        ButtonType okBtn = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
+        alert.getButtonTypes().setAll(okBtn);
+        alert.show();
+    }
+
+    public void setDefault(){
+        phoneTxtField.setText(Controller.selectedRow.getPhoneNumber());
+        birthDatePicker.setValue(Controller.selectedRow.getDateOfBirth());
+        lastNameTxtField.setText(Controller.selectedRow.getLastName());
+        firstNameTxtField.setText(Controller.selectedRow.getFirstName());
+        idTxtField.setText(String.valueOf(Controller.selectedRow.getPersonId()));
+    }
+}
